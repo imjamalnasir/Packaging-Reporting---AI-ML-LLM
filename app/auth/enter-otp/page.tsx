10dc18a9f878a4b2ba7c6d987a60ff7ed9689c0d
Jamal Nasir <jamc_2@hotmail.com>
2026-03-02 12:34:02 +0500

    fix otp

--- app/auth/enter-otp/page.tsx
+++ app/auth/enter-otp/page.tsx
@@ -1,18 +1,20 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 import { EnterOtpGetToken } from "@/components/packiq/auth/enter-otp-get-token";
 
-interface EnterOtpPageProps {
-  searchParams: {
-    method?: string;
-  };
-}
 
-export default function EnterOtpPage({ searchParams }: EnterOtpPageProps) {
-  const method = searchParams?.method ?? "";
+
+export default function EnterOtpPage() {
+
+    const searchParams = useSearchParams();
 
   return (
     <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-1xl">
-        <EnterOtpGetToken otpmethod={method} userId="01" />
+        <EnterOtpGetToken otpmethod={method} />
+       
       </div>
     </div>
   );
